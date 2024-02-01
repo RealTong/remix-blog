7341c6f60f0a261829b71fdf1f68fc3546b132f7
RealTong <i@wst.sh>
2024-02-02 00:29:49 +0800

    feat(page2): page2 done
    page2 done

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,59 @@
-import type { MetaFunction } from "@remix-run/node";
+import {prisma} from "~/prisma.service";
+import {json, Link, useLoaderData, useSearchParams} from "@remix-run/react";
+import {LoaderFunctionArgs} from "@remix-run/node";
+import {Pagination} from "@nextui-org/react";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export const loader = async (c: LoaderFunctionArgs) => {
+  const search = new URL(c.request.url).searchParams
+  const page = Number(search.get('page') || 1)
+
+  const [posts, total] = await prisma.$transaction([
+    prisma.post.findMany({
+      orderBy: {
+        created_at: "desc"
+      },
+      take: 1,
+      skip: (page - 1) * 1
+    }),
+    prisma.post.count()
+  ])
+
+  return json({
+    posts,
+    pageCount: Math.ceil(total / 1)
+  })
+}
 
 export default function Index() {
+  const loaderData = useLoaderData<typeof loader>()
+
+  // 读取 url param
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = Number(searchParams.get('page') || 1)
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutoria
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <div className={'p-12 flex flex-col gap-4'}>
+        {
+          loaderData.posts.map(post => {
+            return (
+              <div key={post.id}>
+                <Link to={`/posts/${post.id}`} className={'text-xl'}>
+                  {post.title}
+                </Link>
+                <div className={'text-sm text-gray-400'}>
+                  {post.created_at}
+                </div>
+              </div>
+            )
+          })
+        }
+        <Pagination total={loaderData.pageCount} page={page} onChange={page => {
+          const newSearchParams = new URLSearchParams(searchParams)
+          newSearchParams.set('page', String(page))
+          setSearchParams(newSearchParams)
+        }}/>
+      </div>
     </div>
   );
 }
